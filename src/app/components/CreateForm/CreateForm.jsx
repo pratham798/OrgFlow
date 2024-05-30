03092fda2463d421d4282ae80a5d11e307f652ee
pratham798 <prathamjamnal7982@gmail.com>
2024-05-30 18:17:35 +0530

    feat: create forms for updating and reassigning entities

--- src/app/components/CreateForm/CreateForm.jsx
+++ src/app/components/CreateForm/CreateForm.jsx
@@ -2,19 +2,32 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import InputField from '../common/FormFields/InputField';
+import SelectField from '../common/FormFields/SelectField';
 
 import styles from './CreateForm.module.css';
 
 const CreateForm = ({selectedEntity, action, entityType, dispatchFn, orgData}) => {
   const dispatch = useDispatch();
+  const getLeadEntities = () => {
+    return orgData.map((entity) => ({
+      value: entity.role_id,
+      label: entity.name,
+      position: entity.position,
+    }))
+  }
+  const entityParent = orgData.find((entity) => entity.role_id === selectedEntity.parent)
   const defaultValues = action === 'Update' ? {
     name: selectedEntity.name,
     phone: selectedEntity.phone,
     email: selectedEntity.email,
     position: selectedEntity.position,
-    parent: selectedEntity.parent,
+    parent: {
+      label: entityParent?.name || 'none',
+      value: entityParent?.value,
+      position: (entityParent?.name ? { position: entityParent.position } : null),
+      },
   } : {};
-  const { register, handleSubmit, formState: { errors } } = useForm({defaultValues});
+  const { register, handleSubmit, control, formState: { errors } } = useForm({defaultValues});
   const onSubmit = (entityData) => {
     entityData['role'] = entityType.toLowerCase();
     if(action === 'Add') entityData['parent']=selectedEntity.role_id;
@@ -25,10 +38,8 @@ const CreateForm = ({selectedEntity, action, entityType, dispatchFn, orgData}) =
     <form onSubmit={handleSubmit(onSubmit)} className={styles.FormContainer}>
       {/* Name Input */}
       <InputField
-        label={'Name:'}
-        register={register}
-        inputName={"name"}
-        inputPattern={{value: /^[A-Za-z]+$/i, message: 'Name must contain only letters'}}
+        label={'Name:'} register={register} inputName={"name"}
+        inputPattern={{value: /^[A-Za-z]+(?: [A-Za-z]+)?$/i, message: 'Name must contain only letters'}}
         error={errors.name}
         placeholder={"Enter Name"}
         required
@@ -37,9 +48,7 @@ const CreateForm = ({selectedEntity, action, entityType, dispatchFn, orgData}) =
         <>
           {/* Phone Number Input */}
           <InputField
-            label={'Phone No:'}
-            register={register}
-            inputName={"phone"}
+            label={'Phone No:'} register={register} inputName={"phone"}
             inputPattern={{value: /^\+?[1-9]\d{1,14}$/, message: 'Phone no. must be valid'}}
             error={errors.phone}
             placeholder={"Enter Phone Number"}
@@ -48,9 +57,7 @@ const CreateForm = ({selectedEntity, action, entityType, dispatchFn, orgData}) =
           
           {/* Email Input */}
           <InputField
-            label={'Email:'}
-            register={register}
-            inputName={'email'}
+            label={'Email:'} register={register} inputName={'email'}
             inputPattern={{value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Email must be valid'}}
             error={errors.email}
             placeholder={"Enter Email"}
@@ -58,12 +65,21 @@ const CreateForm = ({selectedEntity, action, entityType, dispatchFn, orgData}) =
           />
 
           {/* Designation Input */}
-          <InputField
-            label={'Designation:'}
-            register={register}
-            inputName={'position'}
-            error={errors.position}
+          <InputField 
+            label={'Designation:'} register={register} inputName={'position'} error={errors.position}
             placeholder={"Enter Designation"}
+            required
+          />
+        </>
+      )}
+      {action === 'Update' && (
+        <>
+          {/* Assignee Select Input */}
+          <SelectField 
+            label={'Assigned To:'} register={register} inputName={'parent'}
+            error={errors.parent} placeholder={"Select Lead"}
+            control={control}
+            options={getLeadEntities()}
             required
           />
         </>
---
+++ src/app/components/common/FormFields/SelectField/SelectField.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Controller } from 'react-hook-form';
+import classNames from 'classnames';
+import Select from 'react-select'; 
+
+import styles from './SelectField.module.css';
+
+const SelectField = ({label, inputName, required, placeholder, error, control, options}) => {
+  return (
+    <>
+      <div className={styles.InputWrapper}>
+        <label htmlFor={inputName}>{label}</label>
+        <Controller
+          name={inputName}
+          control={control}
+          defaultValue={null}
+          rules={{ required: (required && `"${label} is required"`) }}
+          render={({ field }) => (
+            <Select 
+              className={classNames(styles.Select, {
+                [styles.InputInvalid]: error
+              })}
+              {...field}
+              options={options}
+              getOptionLabel={option => `${option.label}${option?.position ? ` (${option.position})` : ''}`}
+              placeholder={placeholder}
+              isClearable
+            />
+          )}
+        />
+      </div>
+      {error && <p className={styles.InputError}>{error.message}</p>}
+    </>
+  )
+}
+
+export default SelectField;
